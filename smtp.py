fa0425e269c75f1e1cc63d5e0d8d3f2137eeac07
Ihor Pleshchuk <ihor.pleshchuk@gmail.com>
2022-02-07 13:56:30 +0200

    Define methods of class Smtp

--- smtp.py
+++ smtp.py
@@ -9,3 +9,24 @@ class Smtp():
   def __init__(self, server, port = 25):
     self.server = server
     self.port = port
+
+  def set_sender(self, sender):
+    self.sender = sender
+
+  def set_recipients(self, recipients):
+    self.recipients = recipients
+
+  def set_server(self, server):
+    self.server = server
+
+  def set_port(self, port):
+    self.port = port
+
+  def set_subject(self, subject):
+    self.subject = subject
+
+  def get_sender(self):
+    return self.sender
+
+  def get_recipients(self):
+    return self.recipients
